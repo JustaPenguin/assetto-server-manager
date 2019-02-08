df447a5601c371add0d0743737b3270e015836df
Callum Jones <cj@icj.me>
2019-02-08 15:38:44 +0000

    add debug mode for logging, improve layout of race widget on homepage on mobile devices

--- content_tyres.go
+++ content_tyres.go
@@ -43,7 +43,7 @@ func ListTyres() (Tyres, error) {
 		tyres, err := loadTyresFromFile(file)
 
 		if os.IsNotExist(err) {
-			logrus.Warnf("Skipping loading tyre data from %s, file does not exist", file)
+			logrus.Debugf("Skipping loading tyre data from %s, file does not exist", file)
 			continue
 		} else if err != nil {
 			return nil, err
