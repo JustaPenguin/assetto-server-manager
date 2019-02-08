0864c599b41f73824e285658fb4f2343fe9438fa
Callum Jones <cj@icj.me>
2019-02-08 15:59:52 +0000

    add searching for cars via jquery quicksearch

--- cmd/server-manager/static/manager.js
+++ cmd/server-manager/static/manager.js
@@ -91,7 +91,41 @@ let raceSetup = {
         raceSetup.$addWeatherButton = $document.find("#addWeather");
 
         if (raceSetup.$carsDropdown) {
-            raceSetup.$carsDropdown.multiSelect();
+            raceSetup.$carsDropdown.multiSelect({
+                selectableHeader: "<input type='search' class='form-control search-input' autocomplete='off' placeholder='search'>",
+                selectionHeader: "<input type='search' class='form-control search-input' autocomplete='off' placeholder='search'>",
+                afterInit: function(ms){
+                    let that = this,
+                        $selectableSearch = that.$selectableUl.prev(),
+                        $selectionSearch = that.$selectionUl.prev(),
+                        selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+                        selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+                    that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+                        .on('keydown', function(e){
+                            if (e.which === 40){
+                                that.$selectableUl.focus();
+                                return false;
+                            }
+                        });
+
+                    that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+                        .on('keydown', function(e){
+                            if (e.which === 40){
+                                that.$selectionUl.focus();
+                                return false;
+                            }
+                        });
+                },
+                afterSelect: function(){
+                    this.qs1.cache();
+                    this.qs2.cache();
+                },
+                afterDeselect: function(){
+                    this.qs1.cache();
+                    this.qs2.cache();
+                }
+            });
             raceSetup.$tyresDropdown = $document.find("#LegalTyres");
 
             if (raceSetup.$tyresDropdown) {
