2f7bab232b14040f90ea9cb812e2245e32e5632b
Callum Jones <cj@icj.me>
2019-02-15 23:24:43 +0000

    add a very messy but functional utility for recording/replaying UDP messages from a JSON file (with time multiplier)

--- cmd/udp-session-logger/main.go
+++ cmd/udp-session-logger/main.go
@@ -1,63 +1,36 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/cj123/assetto-server-manager/pkg/udp"
+	"github.com/cj123/assetto-server-manager/pkg/udp/replay"
 	"github.com/davecgh/go-spew/spew"
-	"os"
 	"time"
 )
 
-var outFileName = time.Now().Format(time.ANSIC + ".json")
+var outFileName = time.Now().Format("2006-01-02_15.04.json")
 
 func main() {
+	/*	callback := replay.RecordUDPMessages(outFileName)
 
-	udpServerConn, err := udp.NewServerClient("127.0.0.1", 12000, 11000, false, "", callback)
+		udpServerConn, err := udp.NewServerClient("127.0.0.1", 12002, 11002, false, "", callback)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer udpServerConn.Close()
+
+		ch := make(chan struct{})
+
+		<-ch // wait forever*/
+
+	err := replay.ReplayUDPMessages("2019_02_15_20.45.json", 1000, callback)
 
 	if err != nil {
 		panic(err)
 	}
-
-	defer udpServerConn.Close()
-
-	ch := make(chan struct{})
-
-	<-ch // wait forever
 }
-
-type Entry struct {
-	Received  time.Time
-	EventType udp.Event
-
-	Data udp.Message
-}
-
-var entries []Entry
 
 func callback(message udp.Message) {
 	spew.Dump(message)
-
-	entries = append(entries, Entry{
-		Received:  time.Now(),
-		EventType: message.Event(),
-		Data:      message,
-	})
-
-	f, err := os.Create(outFileName)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	encoder := json.NewEncoder(f)
-	encoder.SetIndent("", "  ")
-
-	err = encoder.Encode(entries)
-
-	if err != nil {
-		fmt.Println("err encoding", err)
-	}
 }
