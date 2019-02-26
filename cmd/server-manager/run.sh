e21a1f4f2f3ff7e74a23b56411ad03a42379a2d4
Callum Jones <cj@icj.me>
2019-02-26 15:30:28 +0000

    add an asset helper to prevent caching issues

--- cmd/server-manager/run.sh
+++ cmd/server-manager/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xe
+export BUILD_TIME=$(date +'%s')
 export DEBUG=true
 export GO111MODULE=on
-go run *.go
+go build -ldflags "-s -w -X github.com/cj123/assetto-server-manager.BuildTime=${BUILD_TIME}"
+./server-manager
