7c55c676d51a766e1d46889a43adb94c983e255d
Callum Jones <cj@icj.me>
2019-02-26 15:56:50 +0000

    fix lingering build issues caused by having to subject the project to the wonders of nodejs

--- cmd/server-manager/run.sh
+++ cmd/server-manager/run.sh
@@ -3,5 +3,6 @@ set -xe
 export BUILD_TIME=$(date +'%s')
 export DEBUG=true
 export GO111MODULE=on
+node_modules/.bin/babel javascript/manager.js -o static/manager.js
 go build -ldflags "-s -w -X github.com/cj123/assetto-server-manager.BuildTime=${BUILD_TIME}"
 ./server-manager
