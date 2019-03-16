6a5993e0741d71f7d50359f29470b52f9c226545
Callum Jones <cj@icj.me>
2019-03-16 17:08:00 +0000

    improve process management on windows

--- server_process_windows.go
+++ server_process_windows.go
@@ -2,4 +2,20 @@
 
 package servermanager
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 const serverExecutablePath = "acServer.exe"
+
+func kill(process *os.Process) error {
+	return exec.Command("taskkill", "/F", "/T", "/PID", fmt.Sprint(process.Pid)).Run()
+}
+
+func buildCommand(command string, args ...string) *exec.Cmd {
+	cmd := exec.Command(command, args...)
+
+	return cmd
+}
