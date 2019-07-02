79f15b9925c213725043a1cc63d511df32f539c3
Star_Wraith <albezan72@gmail.com>
2019-07-02 20:44:33 +0200

    3ced5a2343b1ee43630e0dfb02c6f317d669fcee : add error logging for task kill operation

--- server_process_windows.go
+++ server_process_windows.go
@@ -11,11 +11,8 @@ import (
 
 const serverExecutablePath = "acServer.exe"
 
-func kill(process *os.Process) error {
-	//apparently running Process.Kill() and THEN Taskill is a no-go
-	//using only taskkill /t /f works instead, so it's pointless to use Process.Kill() at all - Alberto
-	exec.Command("TASKKILL", "/T", "/F", "/PID", fmt.Sprintf("%d", process.Pid)).Run()
-	return nil
+func kill(process *os.Process) error {	
+	return exec.Command("TASKKILL", "/T", "/F", "/PID", fmt.Sprintf("%d", process.Pid)).Run()
 }
 
 func buildCommand(ctx context.Context, command string, args ...string) *exec.Cmd {
