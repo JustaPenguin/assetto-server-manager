f415ab4133af192d70c8bdd16a393ab4c5c2e7eb
Star_Wraith <albezan72@gmail.com>
2019-07-09 05:02:03 +0200

    File handlers: better error handling and reporting

--- file_handlers.go
+++ file_handlers.go
@@ -11,13 +11,13 @@ import (
 func tryMoveFile(sourcePath string, destPath string) error {	
 	sourceExists, err := pathExists(sourcePath)
 	if err != nil {
-		logrus.Errorf("tryMoveFile() 14: %s", err)
+		logrus.Errorf("couldn't find path %s", sourcePath)
 		return err
 	}
 
 	destExists, err := pathExists(destPath)
 	if err != nil {
-		logrus.Errorf("tryMoveFile() 20: %s", err)
+		logrus.Errorf("couldn't find path %s", destPath)
 		return err
 	}
 
@@ -29,7 +29,7 @@ func tryMoveFile(sourcePath string, destPath string) error {
 				err = os.MkdirAll(filepath.Dir(destPath), 0755)
 			} 
 		} else {
-			logrus.Errorf("tryMoveFile() 32: %s", err)
+			logrus.Errorf("couldn't check path %s", sourcePath)
 			return err
 		}
 
@@ -41,13 +41,13 @@ func tryMoveFile(sourcePath string, destPath string) error {
 			}
 
 			if err != nil{
-				logrus.Errorf("tryMoveFile() 44: %s", err)
+				logrus.Errorf("couldn't move files from %s to %s", sourcePath, destPath)
 				return err
 			} else {
 				logrus.Infof("JSON migration : moved %s to %s", sourcePath, destPath)
 			}
 		} else {
-			logrus.Errorf("tryMoveFile() 50: %s", err)
+			logrus.Errorf("couldn't create directory %s", destPath)
 			return err
 		}
 	}
@@ -63,7 +63,7 @@ func pathExists(path string) (bool, error) {
 	if os.IsNotExist(err) { 
 		return false, nil 
 	} else {
-		logrus.Errorf("pathExists: %s", err)
+		logrus.Errorf("couldn't check for path %s", path)
 	}
 
 	return true, err
@@ -72,14 +72,14 @@ func pathExists(path string) (bool, error) {
 func isDirectory(path string) (bool, error) {
 	exists, err := pathExists(path)
 	if err != nil{
-		logrus.Errorf("isDirectory() 75: %s", err)
+		logrus.Errorf("couldn't check for path %s", path)
 		return false, err
 	}
 
 	if exists {
 		fileInfo, err := os.Stat(path)
 		if err != nil{
-			logrus.Errorf("isDirectory() 82: %s", err)
+			logrus.Errorf("couldn't check for path %s", path)
 			return false, err
 		}
 		return fileInfo.IsDir(), err
@@ -89,13 +89,23 @@ func isDirectory(path string) (bool, error) {
 
 func getFileList(dir string) ([]os.FileInfo, error) {
 	isDir, err := isDirectory(dir)
-	if err != nil || !isDir{
-		logrus.Errorf("listFiles() 93: %s", err)
+
+	if err != nil{
+		logrus.Errorf("trying to get a file list from %s, but it is not a directory", dir)
 		return nil, err
+	}
+
+	if !isDir{
+		return nil, nil
 	}
 
 	files, err := ioutil.ReadDir(dir)
 	var list []os.FileInfo
+
+	if err != nil{
+		logrus.Errorf("couldn't get file list from: %s", dir)
+		return nil, err
+	}
 
 	for _, file := range files {
 		if file.IsDir() {
@@ -111,21 +121,21 @@ func getFileList(dir string) ([]os.FileInfo, error) {
 func rMoveFiles(sourceDir string, destinationDir string) error {	
 	isDir, err := isDirectory(sourceDir)
 	if err != nil || !isDir{
-		logrus.Errorf("rMoveFiles() 114: %s", err)
+		logrus.Errorf("trying to move files from %s, but it is not a directory", sourceDir)
 		return err
 	}
 
 	filesToMove, err  := getFileList(sourceDir)
 
 	if err != nil{
-		logrus.Errorf("rMoveFiles() 121: %s", err)
+		logrus.Errorf("couldn't get file list from: %s", sourceDir)
 		return err
 	}
 
 	destExists, err := pathExists(destinationDir)
 
 	if err != nil{
-		logrus.Errorf("rMoveFiles() 128: %s", err)
+		logrus.Errorf("couldn't check for destination directory: %s", sourceDir)
 		return err
 	}
 
@@ -134,7 +144,7 @@ func rMoveFiles(sourceDir string, destinationDir string) error {
 	}
 
 	if err != nil{
-		logrus.Errorf("rMoveFiles() 137: %s", err)
+		logrus.Errorf("couldn't create destination directory: %s", destinationDir)
 		return err
 	}
 
@@ -145,10 +155,15 @@ func rMoveFiles(sourceDir string, destinationDir string) error {
 		err = os.Rename(filepath.Join(sourceDir,file.Name()), filepath.Join(destinationDir,file.Name()))
 
 		if err != nil{
-			logrus.Errorf("rMoveFiles() 148: %s", err)
+			logrus.Errorf("couldn't move %s to %s", filepath.Join(sourceDir,file.Name()), filepath.Join(destinationDir,file.Name()))
 		}
 	}
 
-	os.Remove(sourceDir)
+	err = os.Remove(sourceDir)
+
+	if err != nil{
+		logrus.Errorf("couldn't delete %s",sourceDir)
+	}
+
 	return nil
 }
