8d2ce13d728e718c2fd5833152f60548dd2b5cd5
Hugh Messenger <hugh.messenger@gmail.com>
2019-08-12 21:38:23 -0500

    Refactoring as per code review

--- notification_manager.go
+++ notification_manager.go
@@ -10,63 +10,46 @@ type NotificationHandler struct {
 	discordManager *DiscordManager
 }
 
-func NewNotificationManager(resolver *Resolver) *NotificationHandler {
+func NewNotificationManager(discord *DiscordManager) *NotificationHandler {
 	return &NotificationHandler{
-		discordManager: resolver.resolveDiscordManager(),
+		discordManager: discord,
 	}
 }
 
 // SendMessage sends a message (surprise surprise)
-func (nm *NotificationHandler) SendMessage(msg string) {
+func (nm *NotificationHandler) SendMessage(msg string) error {
 	var err error
+
+	// Call all message senders here ... atm just discord.  The manager will know if it's enabled or not, so just call it
 	err = nm.discordManager.SendMessage(msg)
 
-	if err != nil {
-		// @TODO log error
-		err = nil
-	}
+	return err
 }
 
 // SendRaceStartMessage sends a message as a race session is started
-func (nm *NotificationHandler) SendRaceStartMessage(config ServerConfig, event RaceEvent) {
-	var err error
+func (nm *NotificationHandler) SendRaceStartMessage(config ServerConfig, event RaceEvent) error {
 	trackInfo, err := GetTrackInfo(config.CurrentRaceConfig.Track, config.CurrentRaceConfig.TrackLayout)
 
-	if err == nil {
-		var msg = fmt.Sprintf("Race %s at %s is starting now", event.EventName(), trackInfo.Name)
-		err = nm.discordManager.SendMessage(msg)
+	if err != nil {
+		return err
 	}
 
-	if err != nil {
-		// @TODO log error
-		err = nil
-	}
+	return nm.SendMessage(fmt.Sprintf("Race %s at %s is starting now", event.EventName(), trackInfo.Name))
 }
 
 // SendRaceReminderMessage sends a reminder a configurable number of minutes prior to a race starting
-func (nm *NotificationHandler) SendRaceReminderMessage(event RaceEvent) {
-	var err error
-	var msg = fmt.Sprintf("Race %s starts in 10 minutes", event.EventName())
-	err = nm.discordManager.SendMessage(msg)
-
-	if err != nil {
-		// @TODO log error
-		err = nil
-	}
+func (nm *NotificationHandler) SendRaceReminderMessage(event RaceEvent) error {
+	return nm.SendMessage(fmt.Sprintf("Race %s starts in 10 minutes", event.EventName()))
 }
 
 // SendRaceReminderMessage sends a reminder a configurable number of minutes prior to a championship race starting
-func (nm *NotificationHandler) SendChampionshipReminderMessage(championship *Championship, event *ChampionshipEvent) {
+func (nm *NotificationHandler) SendChampionshipReminderMessage(championship *Championship, event *ChampionshipEvent) error {
 	var err error
 	trackInfo, err := GetTrackInfo(event.RaceSetup.Track, event.RaceSetup.TrackLayout)
 
-	if err == nil {
-		var msg = fmt.Sprintf("%s race at %s starts in 10 minutes", championship.Name, trackInfo.Name)
-		err = nm.discordManager.SendMessage(msg)
+	if err != nil {
+		return err
 	}
 
-	if err != nil {
-		// @TODO log error
-		err = nil
-	}
+	return nm.SendMessage(fmt.Sprintf("%s race at %s starts in 10 minutes", championship.Name, trackInfo.Name))
 }
