5d02c17a8c115ddeea81d61f61a38e9986eff293
Callum Jones <cj@icj.me>
2019-10-15 14:55:01 +0100

    make encrypt function more generic

--- acsr.go
+++ acsr.go
@@ -35,7 +35,14 @@ func ACSRSendResult(championship Championship) {
 		return
 	}
 
-	encryptedChampionship, err := encryptChampionship(output)
+	key, err := hex.DecodeString(config.ACSR.APIKey)
+
+	if err != nil {
+		logrus.WithError(err).Error("api key in config is incorrect")
+		return
+	}
+
+	encryptedChampionship, err := encrypt(output, key)
 
 	if err != nil {
 		logrus.Error("ACSR output encryption failed")
@@ -67,39 +74,24 @@ func ACSRSendResult(championship Championship) {
 	logrus.Debugf("updated championship: %s sent to ACSR", championship.ID.String())
 }
 
-func encryptChampionship(championshipMarshalled []byte) ([]byte, error) {
-	// encryption
-	key, err := hex.DecodeString(config.ACSR.APIKey)
-
-	if err != nil {
-		logrus.WithError(err).Error("api key in config is incorrect")
-		return nil, err
-	}
-
-	// generate a new aes cipher using our 32 byte long key
+func encrypt(data, key []byte) ([]byte, error) {
 	c, err := aes.NewCipher(key)
+
 	if err != nil {
 		return nil, err
 	}
 
-	// gcm or Galois/Counter Mode, is a mode of operation
-	// for symmetric key cryptographic block ciphers
-	// - https://en.wikipedia.org/wiki/Galois/Counter_Mode
 	gcm, err := cipher.NewGCM(c)
-	// if any error generating new GCM
-	// handle them
+
 	if err != nil {
 		return nil, err
 	}
 
-	// creates a new byte array the size of the nonce
-	// which must be passed to Seal
 	nonce := make([]byte, gcm.NonceSize())
-	// populates our nonce with a cryptographically secure
-	// random sequence
+
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		return nil, err
 	}
 
-	return gcm.Seal(nonce, nonce, championshipMarshalled, nil), nil
+	return gcm.Seal(nonce, nonce, data, nil), nil
 }
