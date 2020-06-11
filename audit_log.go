802fcbb92e51f768d0fedbf065fd6395fcd22cf6
Callum Jones <cj@icj.me>
2020-06-11 11:38:04 +0100

    send udp.NextSession to acServer to make it write out results files

--- audit_log.go
+++ audit_log.go
@@ -93,6 +93,9 @@ func (alh *AuditLogHandler) viewLogs(w http.ResponseWriter, r *http.Request) {
 
 	// render audit log page
 	alh.viewRenderer.MustLoadTemplate(w, r, "server/audit-logs.html", &auditLogTemplateVars{
+		BaseTemplateVars: BaseTemplateVars{
+			WideContainer: true,
+		},
 		AuditLogs: auditLogs,
 	})
 }
